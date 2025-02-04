c8c107979c6d71ca59d9cac289b89a6941175a91
Artur Voronov <artur.voronov.1979@mail.ru>
2025-02-04 21:59:43 +0300

    first file luchano

--- main.py
+++ main.py
@@ -70,6 +70,13 @@ class TunedModel(BaseModel):
         """talls pydantic to convert to even non dict obj to json"""
         orm_mode = True
 
+class ShowUser(TunedModel):
+    user_id: uuid.UUID
+    name: str
+    surname: str
+    email: EmailStr
+    is_active: bool
+
 class UserCreate(BaseModel):
     name = str
     surname = str
@@ -81,3 +88,51 @@ class UserCreate(BaseModel):
             raise HTTPException(
                 status_code=422, detail="Name should contains only letters"
             )
+        return value
+
+    @validator("surname")
+    def validate_surname(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Surname should contains only letters"
+            )
+        return value
+    
+##########################
+# BLOCK WITH API ROUTERS #
+##########################
+
+app = FastAPI(title='luchanos-oxford-university')
+
+user_router = APIRoute()
+
+async def _create_new_user(body: UserCreate) -> ShowUser:
+    async with async_session() as session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            user = await user_dal.create_user(
+                name = body.name,
+                surname = body.surname,
+                email = body.email,
+            )
+            return ShowUser(
+                user_id=user.user_id,
+                name = user.name,
+                surname = user.surname,
+                email = user.email,
+                is_active = user.is_active,
+            )
+
+@user_router.post("/", response_model=ShowUser)
+async def create_user(body: UserCreate) -> ShowUser:
+    return await _create_new_user(body)
+
+# create the instance for the routes
+
+main_api_router = APIRoute()
+
+main_api_router.include_router(user_router, prefix='/user', tags=["user"])
+app.include_router(main_api_router)
+
+if __name__ == "__main":
+    uvicorn.run(app,host="0.0.0.0", port=8000)
